434ba3cbc765234bdcb009ed47063826575ae78d
Deborah Alonso <deborah92ah@hotmail.com>
2022-02-12 23:22:25 +0000

    comenzando aplicaciones de estilos en login

--- hotelbooking/urls.py
+++ hotelbooking/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.booking_list, name='booking_list'),
     path('new_booking', views.new_booking, name='new_booking'),
+    path('booking_contact_data', views.booking_contact_data, name='booking_contact_data'),
+    path('save_booking', views.save_booking, name='save_booking'),
     path('authenticate_user', views.authenticate_user, name='authenticate_user'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('get_room_types_available', views.get_room_types_available, name='get_room_types_available'),
--- hotelbooking/views.py
+++ hotelbooking/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .models import Booking, RoomType, Room
+from django.template import RequestContext
+from .models import Booking, RoomType, Room, Customer
+from datetime import datetime    
+
 
 def login_user(request):
     return render(request, 'registration/login.html', {})
@@ -28,26 +31,64 @@ def booking_list(request):
     bookings = Booking.objects.all()
     return render(request, 'hotelbooking/booking_list.html', {'bookings': bookings})
 
+@login_required(login_url='/accounts/login/')
 def new_booking(request):
-    if request.user.is_authenticated:
-        return render(request, 'hotelbooking/new_booking.html', {})
-    else:
-        return render(request, 'hotelbooking/errors/error_404.html', {})
+    return render(
+        request,
+        'hotelbooking/new_booking.html',
+        {'in_date': str(datetime.now()), 'out_date': str(datetime.now()), 'num_guests': 1}
+    )
 
 def error_404_view(request):
     return redirect('/')
 
 def get_room_types_available(request):
-    #room_types_available = RoomType.objects.all()
     in_date = request.GET['in_date']
     out_date = request.GET['out_date']
     num_guests = request.GET['num_guests']
-    room_types_available = RoomType.objects.filter(max_guest=num_guests, room__is_bookable=True).exclude(booking__checkin_date__lte=out_date, booking__checkout_date__gt=in_date)
+    room_types_available = RoomType.objects.filter(max_guest__gte=num_guests, room__is_bookable=True).exclude(booking__checkin_date__lte=out_date, booking__checkout_date__gt=in_date)
     return render(
         request,
         'hotelbooking/new_booking.html', 
-        {'room_types_available' : room_types_available, 'in_date': in_date, 'out_date': out_date, 'num_guests': num_guests})
+        {'room_types_available' : room_types_available, 'in_date': in_date, 'out_date': out_date, 'num_guests': num_guests}
+    )
 
+def booking_contact_data(request):
+    in_date = request.GET['in_date']
+    out_date = request.GET['out_date']
+    num_guests = request.GET['num_guests']
+    room_type = request.GET['room_type']
+    return render(
+        request,
+        'hotelbooking/booking_contact_data.html',
+        {'in_date': in_date, 'out_date': out_date, 'num_guests': num_guests, 'room_type': room_type}
+    )
 
+def save_booking(request):
+    in_date = request.POST['in_date']
+    out_date = request.POST['out_date']
+    num_guests = request.POST['num_guests']
+    room_type = request.POST['room_type']
+    name = request.POST['name']
+    email = request.POST['email']
+    country_code = request.POST['country_code']
+    phone = request.POST['phone']
+
+    booking_instance = RequestContext(Booking.objects.create(
+        locator = "AAAB123456",
+        room_type = RoomType.objects.get(id=room_type),
+        num_guest = num_guests,
+        customer = Customer.objects.get(id=1),
+        total_price = 300,
+        room_num = Room.objects.get(num=220),
+        checkin_date = in_date,
+        checkout_date = out_date,
+        created_date = str(datetime.now()),
+    ))
+    return render(
+        request,
+        '/',
+        {booking_instance}
+    )
     
 
